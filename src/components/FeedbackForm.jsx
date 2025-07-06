3fa584cfdbb23092572d154741082d231348f954
Love Singhal <groothoon31@gmail.com>
2025-07-06 15:19:00 +0530

    minor improvements

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -30,7 +30,7 @@ const FeedbackForm = () => {
         "https://formbackend-f471.onrender.com/api/v1/user/feedback",
         formData
       );
-      console.log("Feedback Submitted: ", response.data);
+      console.log(response.message);
       toast.success("✅ Feedback submitted successfully!");
 
       // Reset all fields
